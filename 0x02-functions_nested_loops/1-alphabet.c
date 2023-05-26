64a5798d30caa8ce9f9fced1a40b28c10e200c88
khalildahbane <kelldani100@gmial.com>
2023-05-26 07:18:33 -0700

    suffer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 97;n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
