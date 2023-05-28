9822fed5773388d8864980e6cdcb682692b1c05b
khalildahbane <kelldani100@gmial.com>
2023-05-28 13:02:08 -0700

    sighn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,20 +5,21 @@
  * Return: 0
  */
 int print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
-	}
+	_putchar('+');
+	return (1);
+}
 	else if (n < 0)
-        {
-		_putchar('-');
-		return (-1);
-        }
+	{
+	_putchar('-');
+	return (-1);
+	}
 	else
 	{
-		_putchar('0');
-		return (0);
+	_putchar('0');
+	return (0);
 	}
 }
