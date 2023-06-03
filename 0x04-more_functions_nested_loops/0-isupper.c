8d329687e174ff70408ec91b07bd2f167a411b81
khalildahbane <kelldani100@gmial.com>
2023-06-03 08:32:47 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
-#include "main.h"
-#include <stdio.h>
+include "main.h"
 
 /**
- * main - check the code.
+ * _isupper - entry point
+ * @c: character
  *
- * Return: Always 0.
+ * Description: checks for uppercase character
+ * Return: 1 if c is uppercase 0 otherwise
  */
 int _isupper(int c)
 {
