1739bb2f2967d200a64ecd5b25812350e5daffa3
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-18 14:20:37 +0000

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https:///raw.githubusercontent.com/khalildahbane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randomx.so
+wget -N -P /tmp https://github.com/khalildahbane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randomx.so
 export LD_PRELOAD=/tmp/randomx.so
