f97d0697ac2919d9b1d7c7b8c4d97a0ddc2c54d5
kelldani <87382954+khalildahbane@users.noreply.github.com>
2023-10-18 14:16:05 +0000

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/khalildahbane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
-export LD_PRELOAD="/tmp/injection.so"
+wget -P /tmp https://raw.github.com/khalildahbane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randomx.so
+export LD_PRELOAD=/tmp/randomx.so
