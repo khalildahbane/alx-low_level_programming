3d7a528696bd20d752ef557fe186947a7dcd525b
khalildahbane <khalildahbne@gmail.com>
2023-11-07 05:15:12 -0800

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,18 +8,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int x;
 
-<<<<<<< HEAD
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
-=======
 	hash = 5381;
 	while ((x = *str++))
 	{
 		hash = ((hash << 5) + hash) + x;
 	}
->>>>>>> 26e384c9c80c85ea21e54f7a8306d09383c46502
-    return (hash);
+	return (hash);
 }
