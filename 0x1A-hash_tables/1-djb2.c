26e384c9c80c85ea21e54f7a8306d09383c46502
kelldani <kelldani100@gmail.com>
2023-11-07 11:48:59 +0100

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,17 @@
+/**
+ * hash_djb2 - Implementation of the djb2 algorithm
+ * @str: String used to generate hash value
+ * Return: Hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int x;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
+	hash = 5381;
+	while ((x = *str++))
+	{
+		hash = ((hash << 5) + hash) + x;
+	}
     return (hash);
 }
